4fe56d02c61d03e7d6af3e2eb30a034395e5c5aa
xysyty <xyxnusstu@gmail.com>
2023-06-28 16:06:19 +0800

    added checks on sign up form inputs

--- src/main/java/com/project/trs/exception/UserNotFoundException.java
+++ src/main/java/com/project/trs/exception/UserNotFoundException.java
@@ -4,4 +4,8 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(int id) {
         super("Could not find the user with id " + id);
     }
+
+    public UserNotFoundException(String email) {
+        super("Could not find the user with email " + email);
+    }
 }
